078c2ff4576e77127a02d89f8823b52f5b829090
arubiramarin <arubiramarin@gmail.com>
2018-12-03 17:31:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var translate = require('google-translate-api');
+// var translate = require('google-translate-api');
 var port = process.env.PORT || 3800;
 var path = require('path');
+var Translate = require('@google-cloud/translate');
+
+// Your Google Cloud Platform project ID
+const projectId = 'maximal-ship-223810';
+
+// Instantiates a client
+const translate = new Translate({
+    projectId: projectId,
+});
 
 app.use('/', express.static(path.join(__dirname, '.', 'dist')));
 
@@ -14,15 +23,15 @@ app.use('/', express.static(path.join(__dirname, '.', 'dist')));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    translate(msg.message, {from: msg.language, to: 'en'}).then(res => {
+    translate.translate(msg.message, 'en').then(res => {
     	msg.allMessages = {};
-	    msg.allMessages['en'] = res.text;
-	    translate(msg.message, {from: msg.language, to: 'es'}).then(res => {
-		    msg.allMessages['es'] = res.text;
-		    translate(msg.message, {from: msg.language, to: 'fr'}).then(res => {
-			    msg.allMessages['fr'] = res.text;
-			    translate(msg.message, {from: msg.language, to: 'ja'}).then(res => {
-				    msg.allMessages['ja'] = res.text;
+	    msg.allMessages['en'] = res[0];
+	    translate.translate(msg.message, 'es').then(res => {
+		    msg.allMessages['es'] = res[0];
+		    translate.translate(msg.message, 'fr').then(res => {
+			    msg.allMessages['fr'] = res[0];
+			    translate.translate(msg.message,'ja').then(res => {
+				    msg.allMessages['ja'] = res[0];
 				    io.emit('chat message', msg);
 				}).catch(err => {
 				    console.error(err);
